e8077cc5e079d44789b50c176169fbd9afb2cf6f
Matt Foster <fmatthew40@gmail.com>
2021-12-21 01:46:53 -0700

    format script

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,7 +2,6 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-
 // Generate Password Function
 function generatePassword () {
 
@@ -13,7 +12,6 @@ var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var specialcharacters= ["!", "'", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "?", "/", ".", ":", ",", "<", ">", ";", "~", "+", "@", "[", "]", "{", "}", "|" ];
-
 
   // asks user to input amount of characters
 var passLength = window.prompt("Please choose a password length between 8 and 128 characters(enter a number).");
@@ -63,9 +61,7 @@ var passLength = window.prompt("Please choose a password length between 8 and 12
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
